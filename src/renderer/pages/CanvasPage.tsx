fc5c51f36eb2b0b78fcb174dba5b4d6f339a4062
ojuliok <juliobds41@gmail.com>
2026-06-28 14:31:41 -0300

    2.9.1.001 - fixbug index

--- src/renderer/pages/CanvasPage.tsx
+++ src/renderer/pages/CanvasPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useRef, useCallback, useMemo } from 'react';
 
 const isMobileViewport = () => window.innerWidth <= 768;
 import {
